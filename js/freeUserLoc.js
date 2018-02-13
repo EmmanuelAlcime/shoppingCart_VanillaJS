b999860473f7348950b4a35fe5b5fa2928113bd8
Emmanuel Alcime <emmanuelalcime@yahoo.com>
2018-02-12 21:03:59 -0500

    all rcent changes

--- js/freeUserLoc.js
+++ js/freeUserLoc.js
@@ -1,17 +1,28 @@
-(function($){
-    'use strict';
-   
-    var freeIpGEO = {};
-    freeIpGEO.getGEO = function(){
+
+var freeIpGEO = {};
+freeIpGEO.getGEO = function(){
        var xhr = new XMLHttpRequest();
-       var address = "freegeoip.net/json/";
+       var address = "https://freegeoip.net/json/";
        xhr.open("GET", address);
        xhr.responseType = 'json';
        xhr.send();
        xhr.onload = function(){
-         var userLoc = xhr.response;
-         console.log(userLoc);
-         return userLoc;
+         if(xhr.readyState == 3){
+             document.getElementById("pla").innerHTML = "Loading....";
+         }
+        else if(xhr.readyState == 4 && xhr.status == 200){
+           var userLoc = xhr.response;
+           renderCountry(userLoc);
+        }
       };
-    }
-})();
+}
+var thisDiv = document.getElementById("pla");
+var renderCountry = function(rest){
+       thisDiv.appendChild(Object.assign(document.createElement("div"),{
+          id: "countryHolder",
+          width: "100%",
+          height: "3em",
+          innerHTML: "<img src='img/"+rest.country_code+".png'><p>  yes we ship to "+rest.country_name+"</p>"
+     }));
+};
+freeIpGEO.getGEO();
